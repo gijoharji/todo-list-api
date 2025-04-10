cc96edd58fdd6309091c91acc9008d3148601a83
gijoharji <gijoharji@gmail.com>
2025-04-10 14:02:02 +0300

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,1 +1,13 @@
+# celery_worker.py
 
+from celery import Celery
+
+celery_app = Celery(
+    "worker",
+    broker="redis://localhost:6379/0",   # Redis broker
+    backend="redis://localhost:6379/0"   # Redis result backend
+)
+
+@celery_app.task
+def send_notification(task_title: str):
+    print(f"📬 Notification sent for task: {task_title}")
